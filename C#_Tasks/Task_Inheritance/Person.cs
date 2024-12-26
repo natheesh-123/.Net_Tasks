fb671d039ac3b095297a1d476b2ade50355a0c87
natheesh-123 <natheeshkumar4656@gmail.com>
2024-12-26 09:12:14 +0530

    new

---
+++ C#_Tasks/Task_Inheritance/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Person
+    {
+        internal string Name;
+
+      public Person(string name)
+        {
+            this.Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $" {Name}";
+
+        }
+
+    }
+
+    internal class Teacher : Person
+    {
+        
+       public  Teacher(string name) : base(name)
+        {
+            Name = name;
+        }
+        public void Explain()
+        {
+            Console.WriteLine("teacher is explaining");
+        }
+
+
+    }
+
+    internal class Student : Person
+    {
+        public Student(String name): base(name)
+        {
+            Name = name;
+        }
+
+        public void Study()
+        {
+            Console.WriteLine("student is studying");
+        }
+    }
+
+}
---
+++ C#_Tasks/Task_Inheritance/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // string name = 
+           //  Person p= new Person(Console.ReadLine());
+
+            Person[] p1 = new Person[3];
+           // Person p2 = new Teacher();
+           // Person p3 = new Student(); 
+            for(int i=0; i<p1.Length; i++)
+            {
+                if (i == 0)
+                {
+                    string name = Console.ReadLine();
+                    p1[i] = new Teacher(name);
+                }
+                else {
+                    String name= Console.ReadLine();
+                    p1[i] = new Student(name);
+                }
+            }
+
+            for(int j = 0; j < p1.Length; j++)
+            {
+                if (j == 0)
+                {
+                    ((Teacher)p1[j]).Explain();
+                }
+                else {
+                    ((Student)p1[j]).Study();
+                }
+                
+            }
+
+            
+            
+
+        }
+    }
+}
